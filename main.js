a3dd84cdaff91258768d696d6c257cc62eb03dfe
Xavier Li <xavierli4ca@gmail.com>
2023-01-12 12:02:19 -0500

    halfof, multiply

---
+++ main.js
@@ -0,0 +1,6 @@
+//import { halfOf } from './lib.js'
+//console.log(halfOf(84)); //84 is just an example
+
+import { halfOf, multiply} from './lib.js'
+console.log(halfOf(84)); 
+console.log(multiply(21,2));

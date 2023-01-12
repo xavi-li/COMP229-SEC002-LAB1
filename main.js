6b2624384ea312f94df2b705f9ff19a65173e871
Xavier Li <xavierli4ca@gmail.com>
2023-01-12 12:17:49 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 // import { halfOf } from './lib.js'
 // console.log(halfOf(84)); //84 is just an example
 
-// import { halfOf, multiply} from './lib.js'
-// console.log(halfOf(84)); 
-// console.log(multiply(21,2));
+import { halfOf, multiply} from './lib.js'
+console.log(halfOf(84)); 
+console.log(multiply(21,2));
 
 // import doSomething from './doSomething.js'
 // doSomething();
-
-import {flag, touch} from './validator.js'
-console.log{flag};
-touch;
-console.log{flag};
